90c303d64340bb74680b86896ff105970f7a5f78
Ayesha Shaw <ayeshashaw520@gmail.com>
2025-06-10 23:04:18 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,16 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  server: {
-    host: true,
-    port: 5173,
-    proxy: {
-      '/users': {
-        target: 'https://birdie-flappy-bird-game.onrender.com',
-        changeOrigin: true,
-        secure: false,
-      },
+ server: {
+  proxy: {
+    '/users': {
+      target: 'https://birdie-flappy-bird-game.onrender.com',
+      changeOrigin: true,
+      secure: false,
     },
   },
+ }
   plugins: [react()],
 })
